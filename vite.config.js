371474d5cfaec3a7ae94a956acc1cc9f8f5fa8a4
Vladimir Chirikov <vovanchig@gmail.com>
2023-02-16 15:43:18 +0300

    add parallel:24

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 
 export default {
-  plugins: [react(), ssr({ prerender: true })]
+  plugins: [react(), ssr({ prerender: {
+    parallel: 24,
+  } })]
 }
